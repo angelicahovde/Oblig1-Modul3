e282a8240919fc5185cd01a76992231af1ab4e59
Angelica <72790483+angelicahovde@users.noreply.github.com>
2021-03-29 13:10:32 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Oblig1_Modul3_UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
